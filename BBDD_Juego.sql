04b9070405134bf5913673fe2e9e5327de7fa889
OliveiraMLeo <91858113+OliveiraMLeo@users.noreply.github.com>
2021-11-08 11:41:11 +0100

    Add files via upload

---
+++ BBDD_Juego.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS BBDD_JUEGO;
+CREATE DATABASE BBDD_JUEGO;
+
+USE BBDD_JUEGO;
+
+CREATE TABLE Jugador (
+    NOMBRE VARCHAR(60),
+    CONTRASEÑA VARCHAR(20),
+    PRIMARY KEY (NOMBRE)
+);
+
+CREATE TABLE Partida (
+    ID INTEGER,
+    FyH VARCHAR(100),
+    UBICACION VARCHAR(100),
+    DURACION INTEGER,
+    GANADOR VARCHAR(20),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Puente (
+    IDp INTEGER,
+    IDj VARCHAR (60),
+    PUNTUACION INTEGER,
+    FOREIGN KEY (IDp) references Partida(ID),
+    FOREIGN KEY (IDj) references Jugador(NOMBRE)
+);
+
+INSERT INTO Jugador VALUES ('Leo', 2411);
+INSERT INTO Jugador VALUES ('Juan', 2510);
+INSERT INTO Partida VALUES ('1','4 Octubre 12:10','Barcelona', 5, 'Juan');
+INSERT INTO Partida VALUES ('2','5 Octubre 12:20','Barcelona', 5, 'Juan');
+INSERT INTO Partida VALUES ('3','6 Octubre 12:30','Girona', 5, 'Leo');
+INSERT INTO Puente VALUES ('1','Juan',100);
+INSERT INTO Puente VALUES ('2','Juan',100);
+INSERT INTO Puente VALUES ('3','Juan',70);
+INSERT INTO Puente VALUES ('1','Leo',70);
+INSERT INTO Puente VALUES ('2','Leo',90);
+INSERT INTO Puente VALUES ('3','Leo',100);
+
+
